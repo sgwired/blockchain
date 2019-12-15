4608428cdb69b3da840730102565f611eef466f2
Shelton Gayles <sgbythelake@gmail.com>
2019-12-15 13:21:09 -0800

    added hashing

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
-import functools
+from functools import reduce
+import hashlib
+import json
 # The reward for miners creating a new block
 MINING_REWARD = 10
 
@@ -20,10 +22,15 @@ participants = {"Shelton"}
 
 
 def hash_block(block):
-    return "-".join([str(block[key]) for key in block])
+    """ Hashes a block and returns a string representation of it 
+        Arguments:
+            :block: the block that should be hashed
+    """
+    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
 
 
 def get_balance(participant):
+    """ Calculate and return the balance for a participant """
     tx_sender = [
         [tx["amount"] for tx in block["transactions"] if tx["sender"] == participant]
         for block in blockchain
@@ -32,12 +39,12 @@ def get_balance(participant):
         tx["amount"] for tx in open_transactions if tx["sender"] == participant
     ]
     tx_sender.append(open_tx_sender)
-    amount_sent =      functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)    
+    amount_sent =      reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)    
     tx_recipient = [
         [tx["amount"] for tx in block["transactions"] if tx["recipient"] == participant]
         for block in blockchain
     ]
-    amount_received =  functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+    amount_received =  reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
 
     return amount_received - amount_sent
 
@@ -77,6 +84,7 @@ def mine_block():
     last_block = blockchain[-1]
     hashed_block = ""
     hashed_block = hash_block(last_block)
+    print(hashed_block)
     reward_transaction = {
         "sender": "MINING",
         "recipient": owner,
