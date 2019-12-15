d98c96fb78fc25960613c10b3b85a8dfd50ede17
Shelton Gayles <sgbythelake@gmail.com>
2019-12-14 16:13:48 -0800

    added transaction validity

--- blockchain.py
+++ blockchain.py
@@ -83,7 +83,7 @@ def mine_block():
         "amount": MINING_REWARD,
     }
     copied_transactions = open_transactions[:]
-    open_transactions.append(reward_transaction)
+    copied_transactions.append(reward_transaction)
     block = {
         "previous_hash": hashed_block,
         "index": len(blockchain),
@@ -123,6 +123,10 @@ def verify_chain():
     return True
 
 
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transactions])
+
+
 waiting_for_input = True
 while waiting_for_input:
     print("Please choose")
@@ -130,6 +134,7 @@ while waiting_for_input:
     print("2: Mine a new block")
     print("3: Output the blockchain blocks")
     print("4: Output participants")
+    print("5: Check transaction validity")
     print("h: Manipulate the chain")
     print("q: Quit")
     user_choice = get_user_choice()
@@ -148,6 +153,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == "4":
         print(participants)
+    elif user_choice == "5":
+        if verify_transactions():
+            print("All transactions are valid")
+        else:
+            print("There are invalid transactions")
     elif user_choice == "h":
         if len(blockchain) >= 1:
             blockchain[0] = {
