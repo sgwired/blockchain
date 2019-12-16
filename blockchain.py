f333655ca2538e256810df64fb8c5175f1b9220c
Shelton Gayles <sgbythelake@gmail.com>
2019-12-15 18:41:15 -0800

    store and receive blockchain data w/ open transactions

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,7 @@
 from functools import reduce
 import hashlib
 from collections import OrderedDict
-
+import json
 from hash_util import hash_string_256, hash_block
 
 
@@ -26,10 +26,41 @@ owner = "Shelton"
 participants = {"Shelton"}
 
 
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = json.loads(file_content[0][:-1]) # remove the \n character
+        updated_blockchain = []
+        for block in blockchain:
+            updated_block = {
+                'previous_hash': block['previous_hash'],
+                'index': block['index'],
+                'proof': block['proof'],
+                'transactions': [OrderedDict([('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block ['transactions']]
+            }
+            updated_blockchain.append(updated_block)
+        blockchain = updated_blockchain
+        open_transactions = json.loads(file_content[1])
+        updated_transactions = []
+        for tx in open_transactions:
+            updated_transaction = OrderedDict([('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
+            
+            updated_transactions.append(updated_transaction) 
+        open_transactions = updated_transactions
+load_data()
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(json.dumps(blockchain))
+        f.write('\n')
+        f.write(json.dumps(open_transactions))
 
 
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    print(guess)
     guess_hash = hash_string_256(guess)
     print(guess_hash)
     return guess_hash[0:2] == '00'
@@ -43,23 +74,20 @@ def proof_of_work():
         proof += 1
     return proof   
 
+
 def get_balance(participant):
     """ Calculate and return the balance for a participant """
-    tx_sender = [
-        [tx["amount"] for tx in block["transactions"] if tx["sender"] == participant]
-        for block in blockchain
-    ]
-    open_tx_sender = [
-        tx["amount"] for tx in open_transactions if tx["sender"] == participant
-    ]
+    tx_sender = [[tx["amount"] for tx in block["transactions"] 
+        if tx["sender"] == participant] for block in blockchain]
+    open_tx_sender = [tx["amount"] for tx in open_transactions if tx["sender"] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent =      reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)    
-    tx_recipient = [
-        [tx["amount"] for tx in block["transactions"] if tx["recipient"] == participant]
-        for block in blockchain
-    ]
-    amount_received =  reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
-
+    print(tx_sender)
+    amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) 
+        if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)    
+    tx_recipient = [[tx["amount"] for tx in block["transactions"] 
+        if tx["recipient"] == participant] for block in blockchain]
+    amount_received =  reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) 
+        if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
     return amount_received - amount_sent
 
 
@@ -92,6 +120,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -179,6 +208,7 @@ while waiting_for_input:
     elif user_choice == "2":
         if mine_block():
             open_transactions = []
+            save_data()
     elif user_choice == "3":
         print_blockchain_elements()
     elif user_choice == "4":
